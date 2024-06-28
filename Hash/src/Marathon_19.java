ff1db1f30311b569be01c7a5fbb96d82498080b8
SMJin <sstaw@naver.com>
2024-06-28 16:41:52 +0900

    240628 hash_marathon_19

---
+++ Hash/src/Marathon_19.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+public class Marathon_19 {
+    /*
+    완주하지 못한 선수
+
+    많은 선수 중 단 한 명의 선수를 제외하고 모든 선수가 마라톤을 완주하였습니다.
+    마라톤에 참여한 선수들의 이름이 담긴 배열 participant 와
+    완주한 선수들의 이름이 담긴 배열 completion 이 있을 때
+    완주하지 못한 선수의 이름을 반환하는 solution() 함수를 작성하세요.
+
+    [제약 조건]
+    - 마라톤 경기에 참여한 선수 수는 1 명이상 100,000 명 이하입니다.
+    - completion 길이는 participant 길이보다 1 작습니다.
+    - 참가자 이름은 1개 이상 20 개 이하의 알파벳 소문자로 이루어져 있습니다.
+    - 참가자 중에는 동명이인이 있을 수 있습니다.
+
+    [입출력의 예]
+    (1)
+    participant = leo kiki eden
+    completion = eden kiki
+    return leo
+
+    (2)
+    participant = marina josipa nikola vinko filipa
+    completion = josipa filipa marina nikola
+    return vinko
+
+    (3)
+    participant = mislav stanko mislav ana
+    completion = stanko ana mislav
+    return mislav
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] participant = br.readLine().split(" ");
+        String[] completion = br.readLine().split(" ");
+
+        HashMap<String, Integer> participantMap = new HashMap<>();
+
+        for (String p : participant) {
+            if(participantMap.containsKey(p)) {
+                int cnt = participantMap.get(p);
+                participantMap.put(p, ++cnt);
+            } else {
+                participantMap.put(p, 1);
+            }
+        }
+
+        for (String c : completion) {
+            if (participantMap.get(c) == 1) {
+                participantMap.remove(c);
+            } else {
+                int cnt = participantMap.get(c);
+                participantMap.put(c, --cnt);
+            }
+        }
+
+        String[] answer = participantMap.keySet().toArray(new String[0]);
+        System.out.println(answer[0]);
+    }
+}
